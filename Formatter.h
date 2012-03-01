79dc5cdf92b011ae6146f47e40adc8e8f7da4ad4
Anders Olme <anders.olme@gmail.com>
2012-03-01 20:19:40 +0100

    .

---
+++ Formatter.h
@@ -0,0 +1,14 @@
+//
+//  Formatter.h
+//  Hexman
+//
+//  Created by Anders Olme on 2012-03-01.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Hexman_Formatter_h
+#define Hexman_Formatter_h
+
+
+
+#endif
